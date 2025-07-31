6089f05aaa36c91c2fa0a3f3952caf1b37a5c837
Yuta Fujiyama <yuta.technology@gmail.com>
2025-07-31 19:34:53 +0900

    fix gpio warning

--- src/peripheral/gpio.cpp
+++ src/peripheral/gpio.cpp
@@ -56,45 +56,11 @@ bool Gpio::Write(bool output) {
 }
 
 void Gpio::SetAltFunction(AltFunction alt_function) {
-  if (pin_ <= 9) {
-    REG_GPIO->gpfsel0 &= ~(0b111 << pin_ * 3);
-    REG_GPIO->gpfsel0 |= static_cast<uint32_t>(alt_function) << pin_ * 3;
-  } else if (10 <= pin_ && pin_ <= 19) {
-    REG_GPIO->gpfsel1 &= ~(0b111 << (pin_ - 10) * 3);
-    REG_GPIO->gpfsel1 |= static_cast<uint32_t>(alt_function) << (pin_ - 10) * 3;
-  } else if (20 <= pin_ && pin_ <= 29) {
-    REG_GPIO->gpfsel2 &= ~(0b111 << (pin_ - 20) * 3);
-    REG_GPIO->gpfsel2 |= static_cast<uint32_t>(alt_function) << (pin_ - 20) * 3;
-  } else if (30 <= pin_ && pin_ <= 39) {
-    REG_GPIO->gpfsel3 &= ~(0b111 << (pin_ - 30) * 3);
-    REG_GPIO->gpfsel3 |= static_cast<uint32_t>(alt_function) << (pin_ - 30) * 3;
-  } else if (40 <= pin_ && pin_ <= 49) {
-    REG_GPIO->gpfsel4 &= ~(0b111 << (pin_ - 40) * 3);
-    REG_GPIO->gpfsel4 |= static_cast<uint32_t>(alt_function) << (pin_ - 40) * 3;
-  } else if (50 <= pin_ && pin_ <= 57) {
-    REG_GPIO->gpfsel5 &= ~(0b111 << (pin_ - 50) * 3);
-    REG_GPIO->gpfsel5 |= static_cast<uint32_t>(alt_function) << (pin_ - 50) * 3;
-  }
+  SetAltFunction(pin_, alt_function);
 }
 
 void Gpio::SetPullRegister(PullRegister pull_register) {
-  if (pin_ <= 15) {
-    REG_GPIO->pup_pdn_cntrl_reg0 &= ~(0b11 << pin_ * 2);
-    REG_GPIO->pup_pdn_cntrl_reg0 |= static_cast<uint8_t>(pull_register)
-                                    << pin_ * 2;
-  } else if (16 <= pin_ && pin_ <= 31) {
-    REG_GPIO->pup_pdn_cntrl_reg1 &= ~(0b11 << (pin_ - 16) * 2);
-    REG_GPIO->pup_pdn_cntrl_reg1 |= static_cast<uint8_t>(pull_register)
-                                    << (pin_ - 16) * 2;
-  } else if (32 <= pin_ && pin_ <= 47) {
-    REG_GPIO->pup_pdn_cntrl_reg2 &= ~(0b11 << (pin_ - 32) * 2);
-    REG_GPIO->pup_pdn_cntrl_reg2 |= static_cast<uint8_t>(pull_register)
-                                    << (pin_ - 32) * 2;
-  } else if (48 <= pin_ && pin_ <= 57) {
-    REG_GPIO->pup_pdn_cntrl_reg3 &= ~(0b11 << (pin_ - 48) * 2);
-    REG_GPIO->pup_pdn_cntrl_reg3 |= static_cast<uint8_t>(pull_register)
-                                    << (pin_ - 48) * 2;
-  }
+  SetPullRegister(pin_, pull_register);
 }
 
 void Gpio::SetAltFunction(uint8_t pin, AltFunction alt_function) {
@@ -103,25 +69,37 @@ void Gpio::SetAltFunction(uint8_t pin, AltFunction alt_function) {
     return;
   }
   if (pin <= 9) {
-    REG_GPIO->gpfsel0 &= ~(0b111 << pin * 3);
-    REG_GPIO->gpfsel0 |= static_cast<uint32_t>(alt_function) << pin * 3;
+    uint32_t val = REG_GPIO->gpfsel0;
+    val &= ~(0b111 << pin * 3);
+    val |= static_cast<uint32_t>(alt_function) << pin * 3;
+    REG_GPIO->gpfsel0 = val;
   } else if (10 <= pin && pin <= 19) {
-    REG_GPIO->gpfsel1 &= ~(0b111 << (pin - 10) * 3);
-    REG_GPIO->gpfsel1 |= static_cast<uint32_t>(alt_function) << (pin - 10) * 3;
+    uint32_t val = REG_GPIO->gpfsel1;
+    val &= ~(0b111 << (pin - 10) * 3);
+    val |= static_cast<uint32_t>(alt_function) << (pin - 10) * 3;
+    REG_GPIO->gpfsel1 = val;
   } else if (20 <= pin && pin <= 29) {
-    REG_GPIO->gpfsel2 &= ~(0b111 << (pin - 20) * 3);
-    REG_GPIO->gpfsel2 |= static_cast<uint32_t>(alt_function) << (pin - 20) * 3;
+    uint32_t val = REG_GPIO->gpfsel2;
+    val &= ~(0b111 << (pin - 20) * 3);
+    val |= static_cast<uint32_t>(alt_function) << (pin - 20) * 3;
+    REG_GPIO->gpfsel2 = val;
   } else if (30 <= pin && pin <= 39) {
-    REG_GPIO->gpfsel3 &= ~(0b111 << (pin - 30) * 3);
-    REG_GPIO->gpfsel3 |= static_cast<uint32_t>(alt_function) << (pin - 30) * 3;
+    uint32_t val = REG_GPIO->gpfsel3;
+    val &= ~(0b111 << (pin - 30) * 3);
+    val |= static_cast<uint32_t>(alt_function) << (pin - 30) * 3;
+    REG_GPIO->gpfsel3 = val;
   } else if (40 <= pin && pin <= 49) {
-    REG_GPIO->gpfsel4 &= ~(0b111 << (pin - 40) * 3);
-    REG_GPIO->gpfsel4 |= static_cast<uint32_t>(alt_function) << (pin - 40) * 3;
+    uint32_t val = REG_GPIO->gpfsel4;
+    val &= ~(0b111 << (pin - 40) * 3);
+    val |= static_cast<uint32_t>(alt_function) << (pin - 40) * 3;
+    REG_GPIO->gpfsel4 = val;
   } else if (50 <= pin && pin <= 57) {
-    REG_GPIO->gpfsel5 &= ~(0b111 << (pin - 50) * 3);
-    REG_GPIO->gpfsel5 |= static_cast<uint32_t>(alt_function) << (pin - 50) * 3;
+    uint32_t val = REG_GPIO->gpfsel5;
+    val &= ~(0b111 << (pin - 50) * 3);
+    val |= static_cast<uint32_t>(alt_function) << (pin - 50) * 3;
+    REG_GPIO->gpfsel5 = val;
   } else {
-    if (pin > 57) Log(LogLevel::Error, "[Gpio]GPIO %d is not exists.\n", pin);
+    Log(LogLevel::Error, "[Gpio]GPIO %d is not exists.\n", pin);
   }
 }
 
@@ -131,23 +109,27 @@ void Gpio::SetPullRegister(uint8_t pin, PullRegister pull_register) {
     return;
   }
   if (pin <= 15) {
-    REG_GPIO->pup_pdn_cntrl_reg0 &= ~(0b11 << pin * 2);
-    REG_GPIO->pup_pdn_cntrl_reg0 |= static_cast<uint8_t>(pull_register)
-                                    << pin * 2;
+    uint32_t val = REG_GPIO->pup_pdn_cntrl_reg0;
+    val &= ~(0b11 << pin * 2);
+    val |= static_cast<uint8_t>(pull_register) << pin * 2;
+    REG_GPIO->pup_pdn_cntrl_reg0 = val;
   } else if (16 <= pin && pin <= 31) {
-    REG_GPIO->pup_pdn_cntrl_reg1 &= ~(0b11 << (pin - 16) * 2);
-    REG_GPIO->pup_pdn_cntrl_reg1 |= static_cast<uint8_t>(pull_register)
-                                    << (pin - 16) * 2;
+    uint32_t val = REG_GPIO->pup_pdn_cntrl_reg1;
+    val &= ~(0b11 << (pin - 16) * 2);
+    val |= static_cast<uint8_t>(pull_register) << (pin - 16) * 2;
+    REG_GPIO->pup_pdn_cntrl_reg1 = val;
   } else if (32 <= pin && pin <= 47) {
-    REG_GPIO->pup_pdn_cntrl_reg2 &= ~(0b11 << (pin - 32) * 2);
-    REG_GPIO->pup_pdn_cntrl_reg2 |= static_cast<uint8_t>(pull_register)
-                                    << (pin - 32) * 2;
+    uint32_t val = REG_GPIO->pup_pdn_cntrl_reg2;
+    val &= ~(0b11 << (pin - 32) * 2);
+    val |= static_cast<uint8_t>(pull_register) << (pin - 32) * 2;
+    REG_GPIO->pup_pdn_cntrl_reg2 = val;
   } else if (48 <= pin && pin <= 57) {
-    REG_GPIO->pup_pdn_cntrl_reg3 &= ~(0b11 << (pin - 48) * 2);
-    REG_GPIO->pup_pdn_cntrl_reg3 |= static_cast<uint8_t>(pull_register)
-                                    << (pin - 48) * 2;
+    uint32_t val = REG_GPIO->pup_pdn_cntrl_reg3;
+    val &= ~(0b11 << (pin - 48) * 2);
+    val |= static_cast<uint8_t>(pull_register) << (pin - 48) * 2;
+    REG_GPIO->pup_pdn_cntrl_reg3 = val;
   } else {
-    if (pin > 57) Log(LogLevel::Error, "[Gpio]GPIO %d is not exists.\n", pin);
+    Log(LogLevel::Error, "[Gpio]GPIO %d is not exists.\n", pin);
   }
 }
 
